caeb85ee7e81a1f34317c0726e59c7ffe888ac46
IAMDHARMA <dharmadhanush529@gmail.com>
2026-02-14 16:54:23 +0530

    code chaged for audio saving

--- src/models/TTS.py
+++ src/models/TTS.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from elevenlabs.client import ElevenLabs
 from elevenlabs.play import play
 import os
+from datetime import datetime
 
 load_dotenv()
 
@@ -9,11 +10,30 @@ client = ElevenLabs(
     api_key=os.getenv("TT_API_KEY") 
 )
 
-audio = client.text_to_speech.convert(
-    text="i am a supernowa clone ai tutor. What can i do it for you?",
+audio_generator = client.text_to_speech.convert(
+    text=input("Give Text:"),
     voice_id="JBFqnCBsd6RMkjVDRZzb",
     model_id="eleven_multilingual_v2",
     output_format="mp3_44100_128",
 )
 
-play(audio)
+# Convert generator to bytes
+audio_bytes = b"".join(audio_generator)
+
+
+# Folder path
+folder_path = "src/tts audio"
+os.makedirs(folder_path, exist_ok=True)
+
+# Create unique filename using timestamp
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+filename = os.path.join(folder_path, f"conversation_{timestamp}.mp3")
+
+# Save file
+with open(filename, "wb") as f:
+    f.write(audio_bytes)
+
+# Play audio
+play(audio_bytes)
+
+print(f"Saved as {filename}")
